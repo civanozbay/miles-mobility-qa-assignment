2f803c10ab06fe96e777651cb4befe967a15e046
civanozbay <civan417@hotmail.com>
2026-05-24 22:03:15 +0200

    add constants and template of loginTest

--- src/test/java/com/miles/tests/LoginTest.java
+++ src/test/java/com/miles/tests/LoginTest.java
@@ -1,10 +1,7 @@
 package com.miles.tests;
 
 import com.miles.base.BaseTest;
-import com.miles.constants.TextConstants;
 import com.miles.driver.DriverManager;
-import com.miles.utils.ConfigReader;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
